26405d9f483c2c61b2e2edc45c5fc2256d47dd75
aeff60 <ssbunklang@gmail.com>
2023-11-28 21:08:10 +0700

    Added user registration form, connected to API, and improved contact form CSS

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+// import css
+import './App.css';
+
+const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3000/form', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, age }),
+    });
+
+    if (response.ok) {
+      setResult('Form submitted successfully');
+    } else {
+      setResult('Failed to submit form');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="age">Age:</label>
+        <input
+          type="number"
+          id="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+      <div>{result}</div>
+    </form>
+  );
+};
+
+export default Form;
